55cbc301f4e1cf2eb7cda00d54fbafc65934c92b
TomNPython <42147472+TomNPython@users.noreply.github.com>
2018-12-04 13:13:13 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/tomnpython/pen/KrrpzX.
+
+ Press the button before it reaches zero, or see what happens! Made with ReactJS
---
+++ index.js
@@ -0,0 +1,81 @@
+class Countdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Press The Button!',
+      subtitle: 'Before the timer runs out...',
+      display: 20,
+      isOn: true,
+      warning: "Are you ready?"
+    };
+  }
+
+  startSession = () => {
+    setInterval(this.sessionTimer, 1000);
+  };
+
+  sessionTimer = () => {
+    if (this.state.display > 0 && this.state.isOn) {
+      this.setState({
+        display: this.state.display - 1
+      });
+      if (this.state.display < 16) {
+        this.setState({
+          warning: "Not long left!"
+        });
+        document.getElementsByTagName("BODY")[0].style.backgroundColor = "#e8c5c5";
+      }
+      if (this.state.display < 11) {
+        this.setState({
+          warning: "Quick!"
+        });
+        document.getElementsByTagName("BODY")[0].style.backgroundColor = "#dd9494";
+      }
+      if (this.state.display < 6) {
+        this.setState({
+          warning: "Click it now!"
+        });
+        document.getElementsByTagName("BODY")[0].style.backgroundColor =
+          "#e06464";
+      }
+      if (this.state.display === 0) {
+        clearInterval(this.sessionTimer)
+        this.setState({
+          warning: "BOOM!",
+          title: 'Too Late!',
+          subtitle: 'The timer ran out...'
+        });
+        document.getElementsByTagName("BODY")[0].style.backgroundColor =
+          "#e20000";
+        document.getElementsByTagName("BODY")[0].style.color = "white";
+      }
+    }
+  };
+
+  resetSession = () => {
+    this.setState({
+      title: 'Press The Button!',
+      subtitle: 'Before the timer runs out...',
+      display: 20,
+      warning: "Are you ready?",
+      isOn: !this.state.isOn
+    });
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "#ede1e1";
+    document.getElementsByTagName("BODY")[0].style.color = "black";
+  };
+
+  render() {
+    return (
+      <div class="container">
+        <h1>{this.state.title}</h1>
+        <h3>{this.state.subtitle}</h3>
+        <div class='time'>{this.state.display}</div>
+        <button onClick={this.startSession}>Start</button>
+        <button onClick={this.resetSession}>Reset</button>
+        <div class='ready-message'>{this.state.warning}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Countdown />, document.getElementById("app"));
